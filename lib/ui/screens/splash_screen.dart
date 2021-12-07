4c1a4a22581ac61e0d164fb4e1f9d83530dbf57b
Ismael Aquino <ibranco@brasiliano.com.br>
2021-12-06 23:17:12 -0300

    create splash screen

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../app/app.dart';
+import '../../controllers/controllers.dart';
+
+class SplashScreen extends GetView<SplashController> {
+  static const String routeName = "/splash";
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder(
+      tween: Tween<double>(begin: .0, end: 1.0),
+      duration: Duration(
+        seconds: 2,
+      ),
+      onEnd: controller.navigateToHome,
+      builder: (BuildContext context, double value, Widget? child) {
+        return Opacity(
+          opacity: value,
+          child: Image.asset(
+            AppImages.appIcon,
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/ui/widgets/animaflix.dart
+++ lib/ui/widgets/animaflix.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../app/app.dart';
+import '../../cross_cutting/cross_cutting.dart';
+import '../screens/screens.dart';
 
 class Animaflix extends StatelessWidget {
   const Animaflix({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return GetMaterialApp(
+      initialRoute: SplashScreen.routeName,
+      theme: ThemeData(
+        primaryColor: AppColors.primaryColor,
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+      ),
+      getPages: [
+        GetPage(
+          name: SplashScreen.routeName,
+          page: () => SplashScreen(),
+          binding: SplashBinding()
+        ),
+      ],
+    );
   }
 }
