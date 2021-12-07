683962bbb9d9cbe9bf4fa1364853dcb7b9b9335b
Ismael Aquino <ibranco@brasiliano.com.br>
2021-12-06 22:55:53 -0300

    add app layer with app infos

---
+++ lib/app/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFFF0002);
+  static const Color backgroundColor = Color(0xFF050408);
+}
---
+++ lib/app/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static const String allAnimes = 'assets/background_image.jpg';
+  static const String appIcon = 'assets/icon.png';
+}
