f514843493e58360bbf7d3e5ecd4e7bf1a7108ab
Ismael Aquino <ibranco@brasiliano.com.br>
2021-12-13 17:47:20 -0300

    add dynamic background color and hiden status bar on splash screen

--- lib/ui/screens/splash/splash_screen.dart
+++ lib/ui/screens/splash/splash_screen.dart
@@ -3,26 +3,31 @@ import 'package:get/get.dart';
 
 import '../../../app/app.dart';
 import '../../../controllers/controllers.dart';
+import '../../mixins/mixins.dart';
 
-class SplashScreen extends GetView<SplashController> {
+class SplashScreen extends GetView<SplashController> with StatusBarManager {
   static const String routeName = "/splash";
 
   @override
   Widget build(BuildContext context) {
-    return TweenAnimationBuilder(
-      tween: Tween<double>(begin: .0, end: 1.0),
-      duration: Duration(
-        seconds: 2,
+    hidenStatusBar();
+
+    return Material(
+      child: TweenAnimationBuilder(
+        tween: Tween<double>(begin: .0, end: 1.0),
+        duration: Duration(
+          seconds: 2,
+        ),
+        onEnd: controller.navigateToHome,
+        builder: (BuildContext context, double value, Widget? child) {
+          return Opacity(
+            opacity: value,
+            child: Image.asset(
+              AppImages.appIcon,
+            ),
+          );
+        },
       ),
-      onEnd: controller.navigateToHome,
-      builder: (BuildContext context, double value, Widget? child) {
-        return Opacity(
-          opacity: value,
-          child: Image.asset(
-            AppImages.appIcon,
-          ),
-        );
-      },
     );
   }
 }
