b5ea7e74c3acc914ad940fa4321cd4a4f0c3651c
Ismael Aquino <ibranco@brasiliano.com.br>
2021-12-13 23:26:37 -0300

    create anime entity

---
+++ lib/domain/entities/anime_entity.dart
@@ -0,0 +1,17 @@
+class Anime {
+  final String id;
+  final String banner;
+  final List<String> genres;
+  final String name;
+  final double note;
+  final int year;
+
+  Anime({
+    required this.id,
+    required this.banner,
+    required this.genres,
+    required this.name,
+    required this.note,
+    required this.year,
+  });
+}
