7f386ac405146bcc4ab14af66c3b912c5d91328b
Ismael Aquino <ibranco@brasiliano.com.br>
2021-12-15 09:09:23 -0300

    add category and description on anime model

--- lib/data/models/anime_model.dart
+++ lib/data/models/anime_model.dart
@@ -1,17 +1,21 @@
 import '../../domain/entities/entities.dart';
 
 class AnimeModel {
-  final String id;
   final String banner;
+  final String? category;
+  final String? description;
   final List<String> genres;
+  final String id;
   final String name;
   final double note;
   final int year;
 
   AnimeModel({
-    required this.id,
     required this.banner,
+    required this.category,
+    required this.description,
     required this.genres,
+    required this.id,
     required this.name,
     required this.note,
     required this.year,
@@ -21,6 +25,8 @@ class AnimeModel {
     return AnimeModel(
       id: json['_id'],
       banner: json['banner'],
+      category: json['category'],
+      description: json['description'],
       genres: <String>[...json['genres']],
       name: json['name'],
       note: double.parse(json['note'].toString().isEmpty ? '0.0' : json['note']),
@@ -31,6 +37,8 @@ class AnimeModel {
   Anime toEntity() {
     return Anime(
       id: id,
+      category: category,
+      description: description,
       banner: banner,
       genres: genres,
       name: name,
