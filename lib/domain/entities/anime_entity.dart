5c01419f133f6a5808743ca4e5f41ba4569745c4
Ismael Aquino <ibranco@brasiliano.com.br>
2021-12-15 09:06:21 -0300

    add category and description to anime entity

--- lib/domain/entities/anime_entity.dart
+++ lib/domain/entities/anime_entity.dart
@@ -1,15 +1,19 @@
 class Anime {
-  final String id;
   final String banner;
+  final String? category;
+  final String? description;
   final List<String> genres;
+  final String id;
   final String name;
   final double note;
   final int year;
 
   Anime({
-    required this.id,
     required this.banner,
+    required this.category,
+    required this.description,
     required this.genres,
+    required this.id,
     required this.name,
     required this.note,
     required this.year,
