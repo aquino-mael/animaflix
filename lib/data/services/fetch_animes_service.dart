73beeb236194fd20e3874e71f1c50e145b0c5643
Ismael Aquino <ismaellAquino@hotmail.com>
2021-12-20 10:42:53 -0300

    add try catch block on fetch animes service method

--- lib/data/services/fetch_animes_service.dart
+++ lib/data/services/fetch_animes_service.dart
@@ -1,4 +1,5 @@
 import '../../domain/entities/entities.dart';
+import '../../domain/helpers/helpers.dart';
 import '../../domain/services/services.dart';
 import '../http/http.dart';
 import '../models/models.dart';
@@ -12,23 +13,31 @@ class FetchAnimesService implements IFetchAnimesService {
 
   @override
   Future<List<Anime>> fetchAllAnimes() async {
-    final response = await httpClient.request(
-      url: 'https://api-animaflix.herokuapp.com/animes',
-      method: HttpMethod.GET,
-    );
+    try {
+      final response = await httpClient.request(
+        url: 'https://api-animaflix.herokuapp.com/animes',
+        method: HttpMethod.GET,
+      );
 
-    return response['results']
-           .map<Anime>((result) => AnimeModel.fromJson(result).toEntity())
-           .toList();
+      return response['results']
+            .map<Anime>((result) => AnimeModel.fromJson(result).toEntity())
+            .toList();
+    } catch(e) {
+      throw AppFailure('Error ao buscar lista de animes');
+    }
   }
 
   @override
   Future<Anime> fetchAnimeDetails(String animeId) async {
-    final response = await httpClient.request(
-      url: 'https://api-animaflix.herokuapp.com/animes/$animeId',
-      method: HttpMethod.GET,
-    );
+    try {
+      final response = await httpClient.request(
+        url: 'https://api-animaflix.herokuapp.com/animes/$animeId',
+        method: HttpMethod.GET,
+      );
 
-    return AnimeModel.fromJson(response).toEntity();
+      return AnimeModel.fromJson(response).toEntity();
+    } catch (e) {
+      throw AppFailure('Erro ao buscar detalhes do anime!');
+    }
   }
 }
